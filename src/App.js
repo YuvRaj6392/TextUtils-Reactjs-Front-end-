bf7062e12654c3c20ac53688a2ec31614fe90ed6
Yuv Raj <yuvraj.srivastava6392@gmail.com>
2022-09-22 12:35:30 +0530

    made some functionality in textUtils

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react'
+
 import Navbar from './components/Navbar'
+import TextForm from './components/TextForm'
 
 export default function App() {
+  
   return (
     <div>
     <Navbar title="TextUtils" />
+    <div className="container">
+    <TextForm heading="Enter the text to analyze"  />
+    </div>
+    
     </div>
   )
 }
---
+++ src/components/TextForm.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import  { useState } from 'react'
+export default function TextForm(props) {
+    const [text,setText]=useState("")
+    const handleUpClick=(e)=>{
+        let newText=text.toUpperCase();
+        setText(newText)
+    }
+
+    const handleLowClick=(e)=>{
+        let newText=text.toLowerCase();
+        setText(newText)
+    }
+
+
+    const handleOnChange=(e)=>{
+            setText(e.target.value);
+    }
+
+
+  return (
+    <>
+<div className='container'>
+    <h1>{props.heading}</h1>
+<div className="mb-3">
+  <textarea value={text} className="form-control" id="exampleFormControlTextarea1" rows="8" onChange={handleOnChange} ></textarea>
+</div>
+<button className="btn btn-primary" style={{margin:"10px"}} onClick={handleUpClick} >
+    Convert to UpperCase
+</button>
+<button className="btn btn-primary" onClick={handleLowClick} >
+    Convert to UpperCase
+</button>
+    </div>
+
+    <div className='container my-2'>
+        <h2>Your Text Summary</h2>
+        <p>{text.split(" ").length-1} and {text.length} characters</p>
+        <p>{0.008* text.split(" ").length } minutes to read the text </p>
+        <p>Preview</p>
+        <h2>{text}</h2>
+    </div>
+    </>
+  )
+}
